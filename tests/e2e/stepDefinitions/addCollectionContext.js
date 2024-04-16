f48bc95452f33e6c9619e66e22a1df8183852616
prashant-gurung899 <prasantgrg777@gmail.com>
2024-04-16 15:07:23 +0545

    Added tests for collection

---
+++ tests/e2e/stepDefinitions/addCollectionContext.js
@@ -0,0 +1,22 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { expect } = require('@playwright/test');
+const { AddCollectionPage } = require('../pageObjects/AddCollectionPage');
+
+const addCollectionPage = new AddCollectionPage();
+
+When(
+  'user {string} creates new collection with following details',
+  async function (string, dataTable) {
+    const inputData = dataTable.hashes();
+    await addCollectionPage.fillCollectionInputData(inputData);
+  }
+);
+
+Then(
+  'user {string} should be able to add products to the collection',
+  async function (string) {
+    await expect(
+      page.locator(addCollectionPage.addProductSelector)
+    ).toBeVisible();
+  }
+);
