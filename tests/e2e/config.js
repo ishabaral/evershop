edc0d5e3a5ba9945d6b1e67f0e5cd51034f1fec7
ishabaral <baralisha.16@gmail.com>
2024-04-23 16:39:46 +0545

    fix: CI error

--- tests/e2e/config.js
+++ tests/e2e/config.js
@@ -1,7 +1,7 @@
 const config = {
     baseEvershopAdminUrl: process.env.EVERSHOP_ADMIN_URL || 'http://localhost:3000/admin',
     adminEmail: process.env.ADMIN_EMAIL || 'admin@admin.com',
-    adminPassword: process.env.ADMIN_PASSWORD || 'admin1234',
+    adminPassword: process.env.ADMIN_PASSWORD || 'a1234578',
 }
 
 module.exports = config;
